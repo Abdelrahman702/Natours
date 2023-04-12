616b2f7e4ed05b42d7e211fcd12e86e3d0954bbc
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-04-13 00:02:51 +0200

    handling Patch-request

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.listen(port, () => {
 // });
 
 app.get('/api/v1/tours/:id', (req, res) => {
+  // if iwant to make a parameter optional ?id
   console.log(req.params);
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
@@ -53,4 +54,15 @@ app.post('/api/v1/tours', (req, res) => {
       res.status(201).json({ status: 'success', data: { tour: newTour } });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'invalid ID' });
+  } else {
+    res.status(200).json({
+      status: 'success',
+      data: { tour: '<Updated tour....>' },
+    });
+  }
 });
