f9ba7bd0697822f898153cd4c44c7f802855848e
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-04-12 23:13:03 +0200

    handling Post-request

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+app.use(express.json());
 
 const port = 3000;
 
@@ -14,13 +15,17 @@ app.listen(port, () => {
 //     .json({ message: 'Hello from the server side ', app: 'Natours' });
 // });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: { tours },
-  });
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1; //to get the id of the last existing tours
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour); //push the newTour into the tours array (normal push process to array )
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
 });
