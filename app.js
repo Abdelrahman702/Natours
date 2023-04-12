dfe1d7d4ebed5bc8babc70a8d443714e2b1935b1
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-04-12 17:42:21 +0200

    handling Get-request

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
 const port = 3000;
@@ -7,8 +8,19 @@ app.listen(port, () => {
   console.log(`App is running on port ${port}....`);
 });
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side ', app: 'Natours' });
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side ', app: 'Natours' });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
 });
