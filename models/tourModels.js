05edbcedbe602951ab65d9d1c6311cffffd93194
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-04-22 20:36:20 +0200

    query middleware

--- models/tourModels.js
+++ models/tourModels.js
@@ -80,6 +80,12 @@ tourSchema.pre('save', function (next) {
   next(); // it is a middleware so it must have next function or it will be stuck in here
 });
 
+//QUERY MIDDLEWARE
+tourSchema.pre('/^find/', function (next) {
+  // this reqular expression for appling this middleware fora ll find query
+  next(); // it is a middleware so it must have next function or it will be stuck in here
+});
+
 //Model of the schema
 
 const Tour = mongoose.model('Tour', tourSchema);
