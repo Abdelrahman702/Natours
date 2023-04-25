fd59e54f9f5190f3f36fe3bf687e34aacdb60111
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-04-25 05:07:10 +0200

    validate password

--- models/userModel.js
+++ models/userModel.js
@@ -22,6 +22,13 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      //This only work for CREATE and SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passswords are not the same ',
+    },
   },
 });
 
