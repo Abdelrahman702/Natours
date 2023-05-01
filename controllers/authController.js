967654d2954465a6a47946f24583b9e1851cd9b8
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-05-01 21:14:34 +0300

    send cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,22 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  const cookiesOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true, // this make the browser able to receiveand send cookies only not access it
+  };
+  if (process.env.NODE_ENV === 'production') {
+    cookiesOptions.secure = true; // this for sending the cookie in encrypted connection (https)
+  }
+
+  res.cookie('jwt', token, cookiesOptions);
+
+  //Remove the passsword from the output
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
