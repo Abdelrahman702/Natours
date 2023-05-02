9d59c7497467ccea68cf5c514191728e6144ebe8
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-05-02 11:01:40 +0300

    preventing parameter pollution

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const fs = require('fs');
-const app = express();
 const morgan = require('morgan'); // it show the information about the request
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitiza = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+const app = express();
+
 // 1) MIDDLEWARES
+
+//Prevent parameter pollution
+
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+      'ratingsAverage',
+      'ratingsQuantity',
+    ], //to ALLOW DUPLICATES
+  })
+);
+
+//Data sanitization against NOSQL queries
+
+app.use(mongoSanitiza());
+
+//Data sanitization against XSS
+
+app.use(xss());
 
 //Security for http header
 
@@ -25,6 +51,8 @@ if (process.env.NODE_ENV === 'development') {
 
 //Body parser , reading data from req.body
 app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NOSQL query injection
 
 //Limit requests from the same IP
 const limiter = rateLimit({
