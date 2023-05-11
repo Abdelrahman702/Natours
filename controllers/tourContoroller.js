8176d8694d3b7c2777629876321fd57f905b14db
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-05-11 17:36:25 +0300

    Geospatial Queries_ Finding Tours Within Radius

--- controllers/tourContoroller.js
+++ controllers/tourContoroller.js
@@ -1,7 +1,7 @@
 const { query } = require('express');
 const Tour = require('./../models/tourModels');
 const catchAsync = require('./../utils/catchAsync');
-// const AppError = require('./../utils/appError');
+const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
 // const tours = JSON.parse(
@@ -96,6 +96,39 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     status: 'success',
     data: {
       plan,
+    },
+  });
+});
+
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+
+  const [lat, lng] = latlng.split(',');
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'please provide lattitude and longitude in the format lat,lng.',
+        400
+      )
+    );
+  }
+
+  console.log(distance, lat, lng, unit);
+
+  const tours = await Tour.find({
+    startLocation: {
+      $geoWithin: { $centerSphere: [[lng, lat], radius] },
+    },
+  });
+
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      data: tours,
     },
   });
 });
