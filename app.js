da1a454bd1fa21803aee6f94fe98c8e9275ae571
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-05-15 21:50:09 +0300

    Including Files into Pug Templates

--- app.js
+++ app.js
@@ -83,6 +83,14 @@ app.get('/', (req, res) => {
   res.status(200).render('base');
 });
 
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', { title: 'All Tours' });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', { title: 'The Forest Hiker Tour' });
+});
+
 app.use('/api/v1/tours', tourRouter); //use tourRouter to point to this url
 app.use('/api/v1/users', userRouter); //use userouter to point to this url
 app.use('/api/v1/reviews', reviewRouter);
