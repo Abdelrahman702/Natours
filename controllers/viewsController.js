b8385b218526018a8b7afb7557b60800baf2095f
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-06-22 11:59:50 +0300

    Building the Tour Page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,6 +13,15 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   res.status(200).render('overview', { title: 'All Tours', tours });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', { title: 'The Forest Hiker Tour' });
-};
+exports.getTour = catchAsync(async (req, res) => {
+  // 1 ) Get tour data for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  // 2) Build Template
+
+  // 3) Render that template using data from 1)
+
+  res.status(200).render('tour', { title: 'The Forest Hiker Tour', tour });
+});
